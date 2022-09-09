2cc0d1e515e90f2040b2ad63b5fd261f2b72ced7
mariabuelvas21 <112511200+mariabuelvas21@users.noreply.github.com>
2022-09-09 12:50:38 -0500

    Add files via upload

---
+++ Cine.cs
@@ -0,0 +1,43 @@
+﻿public class cine{
+    public static void Main(string[] args){
+       
+        pelicula vertigo=new pelicula();
+        pelicula annabelle=new pelicula();
+        pelicula american_pie=new pelicula();
+       
+        vertigo.setnombre("Vertigo");
+        vertigo.setNaudiencia(218);
+        vertigo.setclasificacion("Suspenso");
+        vertigo.tostring();
+
+        annabelle.setnombre("Annabelle");
+        annabelle.setNaudiencia(313);
+        annabelle.setclasificacion("Terror");
+        annabelle.tostring();
+
+        american_pie.setnombre("American Pie");
+        american_pie.setNaudiencia(386);
+        american_pie.setclasificacion("Risa");
+        american_pie.tostring();
+
+        Console.WriteLine("¿Cual de estas 3 es tu pelicula favorita?");
+          int respuesta=Convert.ToInt32(Console.ReadLine());
+
+        switch (respuesta){
+           case 1:
+            Console.WriteLine("Su pelicula favirita es Vertigo");
+            break;
+           case 2:
+            Console.WriteLine("Su pelicula favirita es Annabelle");
+            break;
+           case 3:
+            Console.WriteLine("Su pelicula favirita es American Pie");
+            break;
+        
+         default: 
+             Console.WriteLine("ERROR");
+             break;
+
+        }
+    }
+}
---
+++ Pelicula.cs
@@ -0,0 +1,26 @@
+﻿public class pelicula{
+  string clasificacion="",nombre="";
+  int Naudiencia;
+  public string getnombre(){
+    return this.nombre;
+  }
+  public void setnombre(string nombre){
+    this.nombre=nombre;
+  }
+  public string getclasificacion(){
+    return this.clasificacion;
+  }
+  public void setclasificacion(string clasificacion){
+    this.clasificacion=clasificacion;
+  }
+  public int getNaudiencia(){
+    return this.Naudiencia;
+  }
+  public void setNaudiencia(int Naudiencia){
+    this.Naudiencia=Naudiencia;
+  }
+  public void tostring(){
+
+    Console.WriteLine("La pelicula "+this.nombre+" de clasificacion "+this.clasificacion+" tiene "+this.Naudiencia+" asistentes");
+  }
+}
